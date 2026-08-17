59d741a45252e5896d0239d05ce892cd434a70cb
zee <zee@example.com>
2026-08-17 14:11:43 +0200

    Remove br from Accept-Encoding to fix cURL 61 error

--- app/Services/JobScraperService.php
+++ app/Services/JobScraperService.php
@@ -34,7 +34,7 @@ class JobScraperService
             'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
             'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'Accept-Language' => 'en-US,en;q=0.9',
-            'Accept-Encoding' => 'gzip, deflate, br',
+            'Accept-Encoding' => 'gzip, deflate',
             'Referer' => 'https://www.google.com/',
             'Sec-Ch-Ua' => '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
             'Sec-Ch-Ua-Mobile' => '?0',
